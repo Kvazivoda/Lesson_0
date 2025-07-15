08160c6e60a8411f273340a6c2af951d80de86cb
Kvazivoda <devaeff2015@yandex.ru>
2025-07-15 14:25:40 +0300

    add new branch with lessonOne

--- src/main/java/org/example/LessoneOne.java
+++ src/main/java/org/example/LessoneOne.java
@@ -1,12 +1,33 @@
 package org.example;
 
-public class LessoneOne {   public static void printThreeWords() {
+/*public class LessoneOne {   public static void printThreeWords() {
     System.out.println("Orange");
     System.out.println("Banana");
     System.out.println("Apple");
 }
 
     public static void main(String[] args) {
-        printThreeWords(); // Вызов метода
+        printThreeWords();
+    }
+} */
+
+
+public class LessoneOne {
+    public static void checkSumSign() {
+        int a = 15;
+        int b = -10;
+
+        int sum = a + b;
+
+        // Проверка суммы и вывод результата
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkSumSign();
     }
 }
